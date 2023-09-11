bdfd4ce93795f51c1bc717ea46326b6ae75e92a5
Isaac Studley <studleyisaac@gmail.com>
2023-09-11 08:41:40 -0400

    fixing file tree

--- Exercises/ch02/ten_mathematical_expressions.cpp
+++ Exercises/ch02/ten_mathematical_expressions.cpp
@@ -7,58 +7,60 @@ int n = 5;
 int m = 7;
 int z = 7;
 cout << "The product of " << n << " and " << m << " is ";
-cout << n * m << "." << endl; //1
+cout << n * m << "." << endl; 
 
 n = 4;
 m = 2;
 cout << "The sum of " << n << " and " << m << " is ";
-cout << n + m << "." << endl; //2
+cout << n + m << "." << endl; 
 
 n = 10;
 m = 2;
 cout << "The difference of " << n << " and " << m << " is ";
-cout << n - m << "." << endl; //3
+cout << n - m << "." << endl; 
 
 n = 10;
 m = 2;
 z = 7;
 cout << "The sum of " << n << " and " << m << " and " << z << " is ";
-cout << n + m + z << "." << endl; //4
+cout << n + m + z << "." << endl; 
 
 n = 10;
 m = 2;
 z = 3;
 cout << "The difference of the sum of " << n << " and " << m << " and " << z << " is ";
-cout << (n + m)-z << "." << endl; //5
+cout << (n + m)-z << "." << endl; 
 
 n = 10;
 m = 2;
 z = 3;
 cout << "The sum of the product of " << n << " and " << m << " and " << z << " is ";
-cout << (n * m)+z << "." << endl; //6
+cout << (n * m)+z << "." << endl; 
 
 n = 2;
 m = 10;
 z = 3;
 cout << "The product of " << n << " and " << m << " and " << z << " is ";
-cout << n * m * z << "." << endl; //7
+cout << n * m * z << "." << endl; 
 
 n = 4;
 m = 5;
 z = 7;
 cout << "The difference of the product of " << n << " and " << m << " and " << z << " is ";
-cout << (n * m) - z << "." << endl; //8
+cout << (n * m) - z << "." << endl; 
 
 n = 10;
 m = 2;
 z = 7;
 cout << "The difference of  " << n << " and " << m << " and " << z << " is ";
-cout << n - m - z << "." << endl; //9
+cout << n - m - z << "." << endl; 
 
 n = 10;
 m = 2;
 z = 7;
 cout << "The quotient of  " << n << " and " << m << " is ";
-cout << n/m << "." << endl; //10
+cout << n/m << "." << endl; 
+
+return 0;
 
 }
