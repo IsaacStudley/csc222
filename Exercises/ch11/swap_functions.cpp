1f8226a19a3c3db28a7d32b3de601d691377831a
Isaac Studley <studleyisaac@gmail.com>
2023-10-02 09:21:52 -0400

    adding ch11 work

---
+++ Exercises/ch11/swap_functions.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+void wont_swap(char a, char b)
+{
+    cout << "a: " << a << "  b: " << b << endl;
+    cout << "address of a: " << long(&a) << endl;
+    cout << "address of b: " << long(&b) << endl;
+    char temp = a;
+    a = b;
+    b = temp;
+    cout << "a: " << a << "  b: " << b << endl;
+}
+
+void will_swap(char& a, char& b)
+{
+    cout << "a: " << a << "  b: " << b << endl;
+    cout << "address of a: " << long(&a) << endl;
+    cout << "address of b: " << long(&b) << endl;
+    char temp = a;
+    a = b;
+    b = temp;
+    cout << "a: " << a << "  b: " << b << endl;
+}
+
+int main()
+{
+    char x = 'x';
+    char y = 'y';
+
+    cout << "x: " << x << "  y: " << y << endl;
+    cout << "address of x: " << long(&x) << endl;
+    cout << "address of y: " << long(&y) << endl;
+    cout << "Calling wont_swap..." << endl;
+    wont_swap(x, y);
+    cout << "x: " << x << "  y: " << y << endl;
+    cout << "Calling will_swap..." << endl;
+    will_swap(x, y);
+    cout << "x: " << x << "  y: " << y << endl;
+
+    return 0;  // It returns different adresses each time
+}
