6b334accaf08f1ea7cfeb8aacf733e26f54bd850
Isaac Studley <studleyisaac@gmail.com>
2023-10-24 09:13:23 -0400

    adding ch13 work

---
+++ Exercises/ch13/playing_card.cpp
@@ -0,0 +1,31 @@
+struct Card
+{
+    int suit, rank;
+
+    Card();
+    Card(int s, int r);
+};
+
+Card::Card() {
+    suit = 0; rank = 0;
+}
+
+Card::Card(int s, int r) {
+    suit = s; rank = r;
+}
+
+bool equals(Card one, Card two)
+{
+    return (one.rank == two.rank && one.suit == two.suit);
+}
+void build_deck(){
+vector<Card> deck(52); // I would like to follow up about this
+    int i = 0;
+for (int suit = 0; suit <= 3; suit++) {
+    for (int rank = 1; rank <= 13; rank++) {
+        deck[i].suit = suit;
+        deck[i].rank = rank;
+        i++;
+    }
+}
+}
