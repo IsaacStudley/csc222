90305d8b38d342d9247617e98093aef19e690251
Isaac <studleyisaac@gmail.com>
2024-02-08 08:26:09 -0500

    forth

---
+++ Exercises/final/game.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+   int _levelData[10][10];
+
+for (int i = 0; i < _levelData.size(); ++i)
+	{
+		for (int j = 0; j < _levelData[i].size(); ++j)
+		{
+			unitTile = _levelData[i][j];
+        }
+
+    }
+}
